a965bbf424395e2ee70b9a4a8ea5b8b30dc1afa5
Erfan <Codewithfed@gmail.com>
2024-01-24 00:11:09 +0330

    getting done with design

---
+++ main.js
@@ -0,0 +1,99 @@
+const inputshow = document.getElementById('input')
+const operationshow = document.querySelector('.operationshow')
+
+// btns
+const btns = [
+    document.getElementById('clean'),
+    document.getElementById('cos'),
+    document.getElementById('sin'),
+    document.getElementById('log'),
+    document.getElementById('power'),
+    document.getElementById('remainder'),
+    document.getElementById('radical'),
+    document.getElementById('add'),
+    document.getElementById('n7'),
+    document.getElementById('n8'),
+    document.getElementById('n9'),
+    document.getElementById('subtract'),
+    document.getElementById('n4'),
+    document.getElementById('n5'),
+    document.getElementById('n6'),
+    document.getElementById('multiply'),
+    document.getElementById('n1'),
+    document.getElementById('n2'),
+    document.getElementById('n3'),
+    document.getElementById('devide'),
+    document.getElementById('n0'),
+    document.getElementById('dot'),
+    document.getElementById('equal'),
+]
+console.log(btns)
+btns[0].addEventListener('click', () => {
+
+})
+btns[1].addEventListener('click', () => {
+
+})
+btns[2].addEventListener('click', () => {
+
+})
+btns[3].addEventListener('click', () => {
+
+})
+btns[4].addEventListener('click', () => {
+
+})
+btns[5].addEventListener('click', () => {
+
+})
+btns[6].addEventListener('click', () => {
+
+})
+btns[7].addEventListener('click', () => {
+
+})
+btns[8].addEventListener('click', () => {
+
+})
+btns[9].addEventListener('click', () => {
+
+})
+btns[10].addEventListener('click', () => {
+
+})
+btns[11].addEventListener('click', () => {
+Z
+})
+btns[12].addEventListener('click', () => {
+
+})
+btns[13].addEventListener('click', () => {
+
+})
+btns[14].addEventListener('click', () => {
+
+})
+btns[15].addEventListener('click', () => {
+
+})
+btns[16].addEventListener('click', () => {
+
+})
+btns[17].addEventListener('click', () => {
+
+})
+btns[18].addEventListener('click', () => {
+
+})
+btns[19].addEventListener('click', () => {
+
+})
+btns[20].addEventListener('click', () => {
+
+})
+btns[21].addEventListener('click', () => {
+
+})
+btns[22].addEventListener('click', () => {
+
+})
